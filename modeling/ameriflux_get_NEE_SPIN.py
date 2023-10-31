a29d8b349dc6d4d4da1a084b02a320c8c3155241
amullen01 <amullen@rangelands-process-development.c.rangelands-explo-1571664594580.internal>
2023-10-31 20:27:53 +0000

    version of ameriflux_get_NEE_SPIN.py used for RCTM v1.0 STARFM calibration

--- modeling/ameriflux_get_NEE_SPIN.py
+++ modeling/ameriflux_get_NEE_SPIN.py
@@ -10,25 +10,29 @@ from sklearn.cluster import KMeans
 from sklearn import preprocessing
 import pickle
 from google.cloud import storage
-import utils
-import RCTM_utils as RCTMU
+import RCTM_utils as rctmu
 import pandas as pd
 import numpy as np
 from datetime import date, datetime, timedelta
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+import sys
+sys.path.insert(1, '../utils')
+import utils
 
 bucket_name='rangelands'
-storage_client = storage.Client.from_service_account_json('/home/amullen/Rangeland-Carbon/remote-sensing/gee_key.json')
+storage_client = storage.Client.from_service_account_json('/home/amullen/Rangeland-Carbon/res/gee_key.json')
 bucket = storage_client.get_bucket(bucket_name)
 
 
 path_to_temp = '/home/amullen/temp/'
-path_to_footprint_list = 'res/sites.txt'
+path_to_footprint_list = '/home/amullen/Rangeland-Carbon/res/site_footprints/sites.txt'
 
 params=[]
-with open('res/RCTM_params.yaml', 'r') as file:
+with open('RCTM_params.yaml', 'r') as file:
   params = yaml.safe_load(file)
+  
+ref_value_calc = 'all'
 
 ### read NEE/spin covariates              
 SPIN_dir='Ameriflux/SPIN_AVG_ALL/'
@@ -41,12 +45,8 @@ NEE_meas_df = NEE_meas_df.rename(columns={'Site': 'site', 'Date': 'time'})
 
 NEE_meas_df['site'] = NEE_meas_df['site'].str[:3]
 
-path_to_clusters='output/fPAR_optimization/clusters/STARFM_fPAR_optimization_vpd_clusters.csv'
-cluster_df = pd.read_csv(path_to_clusters)
-cluster_df['cluster'] = cluster_df['cluster'].astype(int)
-
 NEE_dfs = []
-#loop through site data, calculate reference values, append to df, save to new csv
+
 for filename in utils.gs_listdir(NEE_dir, bucket):
   if filename.endswith('.csv'):
     site_df = pd.read_csv('gs://' + bucket_name + '/' + filename)
@@ -78,113 +78,8 @@ for filename in utils.gs_listdir(SPIN_dir, bucket):
 spin_df = pd.concat(spin_dfs)
 
 ##### Read in vegetation index csvs as pandas dataframes #######
-print('reading in STARFM data')
-df_sites=[]
+df_sites = rctmu.read_in_sites_as_df(path_to_footprint_list, params, bucket_name, ref_value_calc=ref_value_calc)
 
-bad_sites=['Kon']
-
-with open(path_to_footprint_list) as f:
-  sites = [line.rstrip('\n') for line in f]
-  
-  for site in sites:
-    site=site.split('/')[-1]
-    print(site)
-    if site in bad_sites:
-        continue
-    
-    #read in site csv as pandas dataframe with evi and ndvi columns
-    csv_dir = f'Ameriflux_sites/{site}_starfm/covariates_v2/{site}_indices_v2.csv'
-    #csv_dir = f'Ameriflux_sites/{site}_starfm/covariates_smooth_missing/{site}_indices.csv'
-    df=pd.read_csv('gs://' + bucket_name + '/' + csv_dir)
-    df['time'] = pd.to_datetime(df['time'])
-    df['Year'] = df['time'].dt.year
-    #df['evi_sr'] = rctmu.calc_sr(df['evi'])
-    df['ndvi_sr'] = rctmu.calc_sr(df['ndvi'])
-    df['site'] = site
-    
-    #calculate yearly reference values for min and max evi/ndvi
-    df_groupby_yearly = df.groupby(by=['site']).agg({'ndvi':[lambda x: x.quantile(0.02), lambda x: x.quantile(0.98)],
-                                                          'ndvi_sr':[lambda x: x.quantile(0.02), lambda x: x.quantile(0.98)]})
-                                                          #'evi':[lambda x: x.quantile(0.02), lambda x: x.quantile(0.98)],
-                                                          #'evi_sr':[lambda x: x.quantile(0.02), lambda x: x.quantile(0.98)]})
-
-                                                
-    df_groupby_yearly = df_groupby_yearly.reset_index()
-    df_groupby_yearly.columns = ['site', 'ndvi_02_yr', 'ndvi_98_yr', 'ndvi_sr_02_yr', 'ndvi_sr_98_yr']
-                                 #'evi_02_yr', 'evi_98_yr', 'evi_sr_02_yr', 'evi_sr_98_yr']
-    ##### special case sites have combined imagery #####
-    df = pd.merge(df, df_groupby_yearly, on='site').reset_index()
-
-    if site=='Rwe_Rms':
-      rwe=df
-      rwe['site']= 'Rwe'
-      rwe['pft'] = rctmu.get_site_pft('Rwe', params['Ameriflux_site_pfts'])
-      rms=df.copy()
-      rms['site']= 'Rms'
-      rms['pft'] = rctmu.get_site_pft('Rms', params['Ameriflux_site_pfts'])
-      df_sites.append(rwe)
-      df_sites.append(rms)
-      
-    elif site=='ARbc':
-      ARb=df
-      ARb['site']= 'ARb'
-      ARb['pft'] = rctmu.get_site_pft('ARb', params['Ameriflux_site_pfts'])
-      ARc=df.copy()
-      ARc['site']= 'ARc'
-      ARc['pft'] = rctmu.get_site_pft('ARc', params['Ameriflux_site_pfts'])
-      df_sites.append(ARb)
-      df_sites.append(ARc)
-      
-    elif site=='Hn2_3':
-      Hn2=df
-      Hn2['site']= 'Hn2'
-      Hn2['pft'] = rctmu.get_site_pft('Hn2', params['Ameriflux_site_pfts'])
-      Hn3=df.copy()
-      Hn3['site']= 'Hn3'
-      Hn3['pft'] = rctmu.get_site_pft('Hn3', params['Ameriflux_site_pfts'])
-      df_sites.append(Hn2)
-      df_sites.append(Hn3)
-      
-    elif site=='KM2_3':
-      KM2=df
-      KM2['site']= 'KM2'
-      KM2['pft'] = rctmu.get_site_pft('KM2', params['Ameriflux_site_pfts'])
-      KM3=df.copy()
-      KM3['site']= 'KM3'
-      KM3['pft'] = rctmu.get_site_pft('KM3', params['Ameriflux_site_pfts'])
-      df_sites.append(KM2)
-      df_sites.append(KM3)
-      
-    elif site=='LS1_2':
-      LS1=df
-      LS1['site']= 'LS1'
-      LS1['pft'] = rctmu.get_site_pft('LS1', params['Ameriflux_site_pfts'])
-      LS2=df.copy()
-      LS2['site']= 'LS2'
-      LS2['pft'] = rctmu.get_site_pft('LS2', params['Ameriflux_site_pfts'])
-      df_sites.append(LS1)
-      df_sites.append(LS2)
-      
-    elif site=='CZ1_xSJ':
-      df['site']= 'xSJ'
-      df['pft'] = rctmu.get_site_pft('xSJ', params['Ameriflux_site_pfts'])
-      df_sites.append(df)
-      
-    elif site=='Fpe':
-      df['site']= 'FPe'
-      df['pft'] = rctmu.get_site_pft('FPe', params['Ameriflux_site_pfts'])
-      df_sites.append(df)
-      
-    else:
-      df['pft'] = rctmu.get_site_pft(site, params['Ameriflux_site_pfts']) 
-    
-      df_sites.append(df)
-    
-      
-  #merge monthly and yearly reference values back to original df
-  df_sites=pd.concat(df_sites, ignore_index=True)
-
- 
 df_sites['ndvi_02_yr'] = df_sites['ndvi'].quantile(0.02)
 df_sites['ndvi_98_yr'] = df_sites['ndvi'].quantile(0.98)
 df_sites['ndvi_sr_02_yr'] = df_sites['ndvi_sr'].quantile(0.02)
@@ -243,11 +138,6 @@ nee_covariates__grouped.columns = ['site', 'vpd_mean']
 NEE_df = pd.merge(NEE_df, nee_covariates__grouped, on='site').reset_index()
 
 #cluster
-#scaler = preprocessing.StandardScaler().fit(NEE_df['vpd_mean'].to_numpy().reshape(-1, 1))
-#X_scaled = scaler.transform(NEE_df['vpd_mean'].to_numpy().reshape(-1, 1))
-#loaded_model = pickle.load(open('output/fPAR_optimization/clusters/kmeans.pickle', "rb"))
-#NEE_df['cluster'] = loaded_model.predict(X_scaled)
-NEE_df = pd.merge(NEE_df, cluster_df, on='site')
 NEE_df['cluster'] = 0
 #calc fPAR and GPP
 print('calculating fPAR and GPP for dates with data (NEE)')
@@ -287,7 +177,8 @@ spin_df = pd.merge(spin_df, spin_covariates__grouped, on='site').reset_index()
 #X_scaled = scaler.transform(spin_df['vpd_mean'].to_numpy().reshape(-1, 1))
 #loaded_model = pickle.load(open('output/fPAR_optimization/clusters/kmeans.pickle', "rb"))
 #spin_df['cluster'] = loaded_model.predict(X_scaled)
-spin_df = pd.merge(spin_df, cluster_df, on='site')
+##spin_df = pd.merge(spin_df, cluster_df, on='site')
+
 spin_df['cluster'] = 0
 print('caluculating fPAR and GPP for dates with data (spin)')
 spin_df['fPAR_min'] = [params['RCTM_params'][row['pft']]['starfm']['fPAR_min'][row['cluster']] for index, row in spin_df.iterrows()]
@@ -333,7 +224,7 @@ for site in NEE_df['site'].unique():
   NEE_df.loc[NEE_df['site']==site, 'SW_IN_NLDAS'] = NEE_df.loc[NEE_df['site']==site, 'SW_IN_NLDAS'].interpolate()
   
   #interpolate fPAR within 5 observations (25 days)
-  NEE_df.loc[NEE_df['site']==site, 'STARFM_fPAR'] = NEE_df.loc[NEE_df['site']==site, 'STARFM_fPAR'].interpolate(limit=5)
+  NEE_df.loc[NEE_df['site']==site, 'STARFM_fPAR'] = NEE_df.loc[NEE_df['site']==site, 'STARFM_fPAR'].interpolate(limit=50)
   
   #predict fPAR using PCA if gap is longer than 25 days
   if len(NEE_df.loc[(NEE_df['site']==site) & (NEE_df['STARFM_fPAR'].isna()) & (~NEE_df['SWC2'].isna())])>0:
@@ -386,7 +277,7 @@ NEE_df['Year']=NEE_df['time'].dt.year
 NEE_df = NEE_df.sort_values(by=['site', 'time']) 
 NEE_df = NEE_df.rename(columns={'site': 'Site', 'time':'Date'})
 for pft in params['Ameriflux_site_pfts']:
-  NEE_df.loc[NEE_df['Site'].isin(params['Ameriflux_site_pfts'][pft])].to_csv('output/NEE_SPIN/Ameriflux_STARFM_NEE_{}.csv'.format(pft))
+  NEE_df.loc[NEE_df['Site'].isin(params['Ameriflux_site_pfts'][pft])].to_csv('output/NEE/Ameriflux_STARFM_NEE_{}.csv'.format(pft))
 
 ### generate spinup data
 spin_df = NEE_df.loc[(NEE_df['Year']>=2002) & (NEE_df['Year']<=2005)]
@@ -411,4 +302,4 @@ spin_df = spin_df.sort_values(by=['Site', 'Date'])
 
 for pft in params['Ameriflux_site_pfts']:
 
-  spin_df.loc[spin_df['Site'].isin(params['Ameriflux_site_pfts'][pft])].to_csv('output/NEE_SPIN/Ameriflux_STARFM_spin_{}.csv'.format(pft))
+  spin_df.loc[spin_df['Site'].isin(params['Ameriflux_site_pfts'][pft])].to_csv('output/SPIN/Ameriflux_STARFM_spin_{}.csv'.format(pft))
