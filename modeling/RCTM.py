44a2a71685cd5c53e2319da404759c64c208d097
amullen01 <amullen@rangelands-process-development.c.rangelands-explo-1571664594580.internal>
2023-11-13 18:20:49 +0000

    add spinup functionality to RCTM

--- modeling/RCTM.py
+++ modeling/RCTM.py
@@ -7,6 +7,8 @@ sys.path.insert(1, '../utils')
 import utils
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 #google storage init
 bucket_name='rangelands'
@@ -49,31 +51,49 @@ def CarbonModel(C_stocks, covariates, params, days=5, spinup=False):
   
   Tmult, Wmult, Smult, fsdethg, fsdeths, fallrt, rdrg, rdrs, rdr, covariates = InitCarbonModel(params, covariates)
   
-  covariates['Rh'] = (['time', 'y', 'x'], np.zeros(np.shape(covariates['GPP'])))
+  if 'time' in covariates.indexes: 
+    time_index = 'time'
+  elif 'doy_bins_lower' in covariates.indexes: 
+    time_index = 'doy_bins_lower'
+  else: 
+    print('uncertain time index')
+    return
+    
+  covariates['Rh'] = ([time_index, 'y', 'x'], np.zeros(np.shape(covariates['GPP'])))
   covariates['Rh'][:] = np.nan
   
-  for i in range(0, len(covariates.indexes['time'])):
-    print(covariates.indexes['time'][i])
-    doy = pd.DatetimeIndex([pd.to_datetime(str(covariates.indexes['time'][i]))]).dayofyear.values[0]
+  for i in range(0, len(covariates.indexes[time_index])):
+    if spinup==False:
+      print(covariates.indexes[time_index][i])
+    
+    if time_index == 'time':
+      doy = pd.DatetimeIndex([pd.to_datetime(str(covariates.indexes['time'][i]))]).dayofyear.values[0]
+    
+    if time_index == 'doy_bins_lower':
+      doy = covariates.indexes[time_index][i]
+      
     if doy < 230:
       fsdeth = fsdethg[i]
     else: 
       fsdeth = fsdeths  #if before 46th period, fsdeth equals 'snsd' param, else fsdethg equals function of soil moisture
       
-    if spinup and i==0:
-      C_stocks['AGB'] = C_stocks['AGB'] * (1 - fsdeth*fallrt) + covariates['NPP'][i] * (1.0 - params['F_Root']) #subtract root fraction from NPP
-      C_stocks['BGB'] = C_stocks['BGB'] * (1 - rdr[i]) + covariates['NPP'][i] * params['F_Root'] #root fraction parameterization to get belowground biomass
+    #if spinup and i==0:
+    
+    #  print(np.nanmean(C_stocks['AGB']))
+    #  C_stocks['AGB'] = C_stocks['AGB'] * (1 - fsdeth*fallrt) + covariates['NPP'][i] * (1.0 - params['F_Root']) #subtract root fraction from NPP
+    #  C_stocks['BGB'] = C_stocks['BGB'] * (1 - rdr[i]) + covariates['NPP'][i] * params['F_Root'] #root fraction parameterization to get belowground biomass
+    #  print(np.nanmean(C_stocks['AGB']))
       
-      ddt_C_AGL = C_stocks['AGB'] * fsdeth * fallrt - (Tmult[i] * Wmult[i] * params['k_AGL'] * days * C_stocks['AGL']) #aboveground litter
-      ddt_C_BGL = C_stocks['BGB'] * rdr[i] - (Tmult[i] * Wmult[i] * params['k_BGL'] * days * C_stocks['BGL']) #belowground litter
+    #  ddt_C_AGL = C_stocks['AGB'] * fsdeth * fallrt - (Tmult[i] * Wmult[i] * params['k_AGL'] * days * C_stocks['AGL']) #aboveground litter
+    #  ddt_C_BGL = C_stocks['BGB'] * rdr[i] - (Tmult[i] * Wmult[i] * params['k_BGL'] * days * C_stocks['BGL']) #belowground litter
         
-    else:
-      ddt_C_AGL = -1*(Tmult[i] * Wmult[i] * params['k_AGL'] * C_stocks['AGL'] * days) + C_stocks['AGB'] * fsdeth * fallrt #aboveground litter
-      ddt_C_BGL = C_stocks['BGB'] * rdr[i] - (Tmult[i] * Wmult[i] * params['k_BGL'] * C_stocks['BGL'] * days) #belowground litter
-      
-      C_stocks['AGB'] = C_stocks['AGB'] * (1 - fsdeth*fallrt) + covariates['NPP'][i] * (1.0 - params['F_Root']) #subtract root fraction from NPP
-      C_stocks['BGB'] = C_stocks['BGB'] * (1 - rdr[i]) + covariates['NPP'][i] * params['F_Root'] #root fraction parameterization to get belowground biomass
-      
+    
+    ddt_C_AGL = -1*(Tmult[i] * Wmult[i] * params['k_AGL'] * C_stocks['AGL'] * days) + C_stocks['AGB'] * fsdeth * fallrt #aboveground litter
+    ddt_C_BGL = C_stocks['BGB'] * rdr[i] - (Tmult[i] * Wmult[i] * params['k_BGL'] * C_stocks['BGL'] * days) #belowground litter
+    #print(np.nanmean(C_stocks['BGB']))  
+    C_stocks['AGB'] = C_stocks['AGB'] * (1 - fsdeth*fallrt) + covariates['NPP'][i] * (1.0 - params['F_Root']) #subtract root fraction from NPP
+    C_stocks['BGB'] = C_stocks['BGB'] * (1 - rdr[i]) + covariates['NPP'][i] * params['F_Root'] #root fraction parameterization to get belowground biomass
+    #print(np.nanmean(C_stocks['BGB']))  
                                           
     ddt_C_POC = (Tmult[i] * Wmult[i] * params['k_AGL'] * days * params['CSE_AGL'] * C_stocks['AGL'] + Tmult[i] * Wmult[i] * params['k_BGL'] * days * params['CSE_BGL'] * C_stocks['BGL']) - Tmult[i] * Wmult[i] * Smult[i] * params['k_POC'] * days * C_stocks['POC']
       
@@ -90,8 +110,8 @@ def CarbonModel(C_stocks, covariates, params, days=5, spinup=False):
     R_BGL = C_stocks['BGL'] * (Tmult[i] * Wmult[i] * params['k_BGL'] * days * (1-params['CSE_BGL']))
     R_POC = C_stocks['POC'] * (Tmult[i] * Wmult[i] * Smult[i] * params['k_POC'] * days * (1-params['CSE_POC']))
     R_HOC = C_stocks['HOC'] * (Tmult[i] * Wmult[i] * Smult[i] * params['k_HOC'] * days * (1-params['CSE_HOC']))
-      
-    covariates['Rh'][i].values = (-1) * (R_AGL + R_BGL + R_POC + R_HOC) 
+
+    covariates['Rh'][i] = (-1) * (R_AGL + R_BGL + R_POC + R_HOC)
   
   covariates['Ra'] = covariates['NPP'] - covariates['GPP'] 
   covariates['NEE'] = covariates['GPP'] + (covariates['Rh'] + covariates['Ra'])
@@ -104,7 +124,7 @@ def CarbonModel(C_stocks, covariates, params, days=5, spinup=False):
     
   return C_stocks, covariates
 
-def RCTM(C_stocks, covariates, params, spinup_years=0, spin_log_period=1):
+def spinup_RCTM(C_stocks, covariates, params, spinup_years=0, spin_log_period=5):
   
   covariates['ndvi_sr'] = rctmu.calc_sr(covariates['ndvi'])
   covariates['fpar'] = rctmu.calc_fPAR(covariates['ndvi'], params['ndvi_02'], params['ndvi_98'], covariates['ndvi_sr'], params['ndvi_sr_02'], params['ndvi_sr_98'], params['fPAR_min'], params['fPAR_max'])
@@ -115,35 +135,132 @@ def RCTM(C_stocks, covariates, params, spinup_years=0, spin_log_period=1):
   covariates['sm2'] =  covariates['sm2'] * 100
   covariates['tsoil'] = covariates['tsoil'] + 273.15
   
-  C_stock_hist = []
+  AGB_stock_hist = []
+  AGL_stock_hist = []
+  BGB_stock_hist = []
+  BGL_stock_hist = []
+  POC_stock_hist = []
+  HOC_stock_hist = []
+  
+  hist_years = []
   
   for spinup_year in range(0,spinup_years):
-    C_stocks, covariates = CarbonModel(C_stocks, covariates, params, days=5, spinup=False)
-    if spinup_years > 0:
-      if spinup_year % spin_log_period == 0:
-        C_stock_hist.append(np.nanmean(C_stocks['AGB']))
+    
+    if (spinup_year % spin_log_period == 0) & (spinup_year!=0):
+        mean_AGB = np.nanmean(C_stocks['AGB'])
+        AGB_stock_hist.append(mean_AGB)
+        
+        mean_AGL = np.nanmean(C_stocks['AGL'])
+        AGL_stock_hist.append(mean_AGL)
+        
+        mean_BGB = np.nanmean(C_stocks['BGB'])
+        BGB_stock_hist.append(mean_BGB)
+        
+        mean_BGL = np.nanmean(C_stocks['BGL'])
+        BGL_stock_hist.append(mean_BGL)
+        
+        mean_POC = np.nanmean(C_stocks['POC'])
+        POC_stock_hist.append(mean_POC)
+        
+        mean_HOC = np.nanmean(C_stocks['HOC'])
+        HOC_stock_hist.append(mean_HOC)
+        
+        hist_years.append(spinup_year)
+        print(f'year {spinup_year}:, AGB: {mean_AGB}, AGL: {mean_AGL}, BGB: {mean_BGB}, BGL: {mean_BGL}, POC: {mean_POC}, HOC: {mean_HOC}')
+        
+    C_stocks, covariates = CarbonModel(C_stocks, covariates, params, days=5, spinup=True)
+        
+  C_stock_hist = {'year': hist_years, 'AGB': AGB_stock_hist, 'AGL': AGL_stock_hist, 'BGB': BGB_stock_hist, 'BGL': BGL_stock_hist, 'POC': POC_stock_hist, 'HOC': HOC_stock_hist} 
         
   return covariates, C_stocks, C_stock_hist
 
+def RCTM(C_stocks, covariates, params, spinup_years=0, spin_log_period=1):
+  print('Running RCTM')
+  print('calculating GPP')
+  covariates['ndvi_sr'] = rctmu.calc_sr(covariates['ndvi'])
+  covariates['fpar'] = rctmu.calc_fPAR(covariates['ndvi'], params['ndvi_02'], params['ndvi_98'], covariates['ndvi_sr'], params['ndvi_sr_02'], params['ndvi_sr_98'], params['fPAR_min'], params['fPAR_max'])
+  covariates['GPP'] = rctmu.calcGPP(params, covariates['tsoil'], covariates['sm2'], covariates['vpd'], covariates['shortwave_radition'], covariates['fpar'])
+  
+  #Change unit to match with C modeling
+  covariates['sm1'] =  covariates['sm1'] * 100
+  covariates['sm2'] =  covariates['sm2'] * 100
+  covariates['tsoil'] = covariates['tsoil'] + 273.15
+  
+  print('NEE model')
+  C_stocks, covariates = CarbonModel(C_stocks, covariates, params, days=5, spinup=False)   
+  
+  return covariates, C_stocks
+
+if __name__ == "__main_off__":
+
+  parser=argparse.ArgumentParser()
+  
+  parser.add_argument("--starfm_in_dir", help="directory to output STARFM")
+  parser.add_argument("--covariate_in_dir", help="directory to output STARFM")
+  parser.add_argument("--out_dir", help="directory to output STARFM")
+  parser.add_argument("--covariate_nc_outname", help="directory to output STARFM")
+  parser.add_argument("--covariate_csv_outname", help="directory to output STARFM")
+  parser.add_argument("--spin_nc_outname", help="directory to output STARFM")
+  parser.add_argument("--landcover_in_dir", help="directory to output STARFM")
+  parser.add_argument("--param_file", help="directory to output STARFM")
+  parser.add_argument("--param_outname", help="directory to output STARFM")
+  parser.add_argument("--bucket_name", help="bucket name")
+
+  args=parser.parse_args()
+  
+  #Define parameters - these are calibrated parameters for RCTM v1.0
+  param_path = 'gs://rangelands/Ranch_Runs/HB/covariates_nc/HB_params.tif'
+  params = rxr.open_rasterio(param_path, masked=True)
+  params = rctmu.xr_dataset_to_data_array(params)
+  
+  spin_cov_path = 'gs://rangelands/Ranch_Runs/HB/covariates_nc/HB_covariates_spin.nc'
+  spin_covariates = rxr.open_rasterio(spin_cov_path, masked=True)
+  
+  
+  #Define initial C pools
+  C_stocks = {'AGB' : np.full(np.shape(spin_covariates['ndvi'].values[0]), 200),
+              'BGB' : np.full(np.shape(spin_covariates['ndvi'].values[0]), 700),
+              'AGL' : np.full(np.shape(spin_covariates['ndvi'].values[0]), 200),
+              'BGL' : np.full(np.shape(spin_covariates['ndvi'].values[0]), 700),
+              'POC' : np.full(np.shape(spin_covariates['ndvi'].values[0]), 1000),
+              'HOC' : np.full(np.shape(spin_covariates['ndvi'].values[0]), 5000)
+              } 
+            
+
+  spin_covariates, C_stocks, C_stock_hist = spinup_RCTM(C_stocks, spin_covariates, params, spinup_years=100)
+  
+  fig, axes = plt.subplots(6,1, figsize=(5, 18))
+  
+  sns.lineplot(x=C_stock_hist['year'], y=C_stock_hist['AGB'], ax=axes[0], label = 'AGB')
+  sns.lineplot(x=C_stock_hist['year'], y=C_stock_hist['AGL'], ax=axes[1], label = 'AGL')
+  sns.lineplot(x=C_stock_hist['year'], y=C_stock_hist['BGB'], ax=axes[2], label = 'BGB')
+  sns.lineplot(x=C_stock_hist['year'], y=C_stock_hist['BGL'], ax=axes[3], label = 'BGL')
+  sns.lineplot(x=C_stock_hist['year'], y=C_stock_hist['POC'], ax=axes[4], label = 'POC')
+  sns.lineplot(x=C_stock_hist['year'], y=C_stock_hist['HOC'], ax=axes[5], label = 'HOC')
+  
+  plt.savefig('output/RCTM/spinup_stocks_iter_2.jpg', dpi=300)
+  
+  utils.image_average_variables(spin_covariates, ['GPP'], time_index = 'doy_bins_lower', plot_dir='output/RCTM')
+  
 #Define parameters - these are calibrated parameters for RCTM v1.0
-param_path = 'gs://rangelands/Ameriflux_sites/A32_starfm/landcover/params_NLCD_2019.tif'
+print('reading in params')
+param_path = 'gs://rangelands/Ranch_Runs/HB/covariates_nc/HB_params.tif'
 params = rxr.open_rasterio(param_path, masked=True)
 params = rctmu.xr_dataset_to_data_array(params)
 
-spin_cov_path = 'gs://rangelands/Ameriflux_sites/A32_starfm/covariates_v2/A32_covariates_site_test.nc'
-covariates = rxr.open_rasterio(spin_cov_path, masked=True)
+print('reading in covariates') 
+cov_path = 'gs://rangelands/Ranch_Runs/HB/covariates_nc/HB_covariates.nc'
+covariates = rxr.open_rasterio(cov_path, masked=True)
 
-#Define initial C pools
-C_stocks = {'AGB' : np.full(np.shape(covariates['ndvi'].values[0]), 200),
-            'BGB' : np.full(np.shape(covariates['ndvi'].values[0]), 700),
-            'AGL' : np.full(np.shape(covariates['ndvi'].values[0]), 200),
-            'BGL' : np.full(np.shape(covariates['ndvi'].values[0]), 700),
-            'POC' : np.full(np.shape(covariates['ndvi'].values[0]), 1000),
-            'HOC' : np.full(np.shape(covariates['ndvi'].values[0]), 5000)
-            } 
+C_stocks = {'AGB' : np.full(np.shape(covariates['ndvi'].values[0]), 4.18),
+            'BGB' : np.full(np.shape(covariates['ndvi'].values[0]), 30.8),
+            'AGL' : np.full(np.shape(covariates['ndvi'].values[0]), 755),
+            'BGL' : np.full(np.shape(covariates['ndvi'].values[0]), 1885),
+            'POC' : np.full(np.shape(covariates['ndvi'].values[0]), 3473),
+            'HOC' : np.full(np.shape(covariates['ndvi'].values[0]), 5161)
+            }
 
-covariates, C_stocks, C_stock_hist = RCTM(C_stocks, covariates, params, spinup_years=5)
+covariates, C_stocks = RCTM(C_stocks, covariates, params, spinup_years=100)
+  
+utils.image_average_variables(covariates, ['GPP', 'Rh', 'Ra', 'NEE', 'NPP'], plot_dir='output/RCTM/transient')
 
-print(C_stock_hist)
-
-utils.image_average_variables(covariates, ['GPP'], plot_dir='output/RCTM')
