57917fa6421e5430d85722caa75715b2dc2d152c
amullen01 <amullen@rangelands-process-development.c.rangelands-explo-1571664594580.internal>
2024-03-02 00:17:03 +0000

    spatial smoothing function

--- RCTM/modeling/RCTM_postprocessing.py
+++ RCTM/modeling/RCTM_postprocessing.py
@@ -6,9 +6,10 @@ import sys
 sys.path.insert(1, '../utils')
 import utils
 from datetime import datetime
+import numpy as np
 
 bucket_name='rangelands'
-storage_client = storage.Client.from_service_account_json('/home/amullen/Rangeland-Carbon/res/gee_key.json')
+storage_client = storage.Client.from_service_account_json('/home/amullen/res/gee_key.json')
 bucket = storage_client.get_bucket(bucket_name)
 path_to_temp = '/home/amullen/temp/'
 
@@ -22,7 +23,25 @@ path_to_temp = '/home/amullen/temp/'
 #covariate_save_path = 'Ranch_Runs/HB/results/HB_covariate_hist_weighted.nc'
 #C_stock_save_path = 'Ranch_Runs/HB/results/HB_C_stock_hist_weighted.nc'
 
-def average_end_members(grass_path, shrub_path, tree_path, RAP_in_dir, save_path, columns=None):
+def average_end_members(grass_path, shrub_path, tree_path, RAP_in_dir, save_path, bucket, columns=None):
+  """
+  RCTM v1 calibrated parameters can produce visible discontinuities between pixel outputs from different pfts. 
+  The discontinuities are most visible between grass-tree and other pfts. To alleviate this it is often advantageous 
+  to run the model for all PFTs, and create a weighted average of outputs using the RAP landcover fractions. This function creates a
+  weighted averaging of results from end members based on RAP cover fractions.
+  
+  Args:
+  grass_path (str): path to grassland netcdf
+  shrub_path (str): path to grass-shrub netcdf 
+  tree_path (str): path to grass-tree netcdf 
+  RAP_in_dir (str): path to RAP landcover (.tif)
+  save_path (str): path to save result netcdf on google cloud bucket
+  columns list(str): None or subset of data variable names to process
+
+  Returns:
+     None
+  
+  """
   
   if columns is None:
     print('processing all data variables')
@@ -60,39 +79,52 @@ def average_end_members(grass_path, shrub_path, tree_path, RAP_in_dir, save_path
   utils.gs_write_blob(os.path.join(path_to_temp, 'temp_average_write.nc'), save_path, bucket)
   
   return
-
-roi_file='/home/amullen/Rangeland-Carbon/res/site_footprints/HLD_tiles.txt'
-
-with open(roi_file) as f:
-  paths = [line.rstrip('\n') for line in f]
   
-  for path in paths:
-    
-    site='/'.join(path.split('/')[-2:])
-    
-    if site in ['HLD/G1', 'HLD/G2', 'HLD/G3', 'HLD/G4', 'HLD/G5', 'HLD/G6', 'HLD/G4', 'HLD/F1', 'HLD/F2', 'HLD/F3', 'HLD/F4', 'HLD/F5', 'HLD/F6', 
-                'HLD/E1', 'HLD/E2', 'HLD/E3', 'HLD/E4', 'HLD/E5', 'HLD/E6', 'HLD/D2', 'HLD/D3', 'HLD/D4','HLD/D5', 'HLD/C2', 'HLD/C3',
-                'HLD/C4', 'HLD/C5', 'HLD/C6', 'HLD/B3', 'HLD/B4', 'HLD/B5', 'HLD/B6', 'HLD/A3', 'HLD/A4']:
-      continue
-      
-    print(site)
-    
-    RAP_in_dir = f'gs://rangelands/Ranch_Runs/{site}/landcover/RAP_2019.tif'
-    
-    C_stocks_grass_path = f'gs://rangelands/Ranch_Runs/{site}/results/C_stocks_hist_grassland.nc'
-    covariates_grass_path = f'gs://rangelands/Ranch_Runs/{site}/results/flux_hist_grassland.nc'
-    
-    C_stocks_shrub_path = f'gs://rangelands/Ranch_Runs/{site}/results/C_stocks_hist_grass-shrub.nc'
-    covariates_shrub_path = f'gs://rangelands/Ranch_Runs/{site}/results/flux_hist_grass-shrub.nc'
-    
-    C_stocks_tree_path = f'gs://rangelands/Ranch_Runs/{site}/results/C_stocks_hist_grass-tree.nc'
-    covariates_tree_path = f'gs://rangelands/Ranch_Runs/{site}/results/flux_hist_grass-tree.nc'
-    
-    covariate_save_path = f'Ranch_Runs/{site}/results/flux_hist_weighted.nc'
-    C_stock_save_path = f'Ranch_Runs/{site}/results/C_stock_hist_weighted.nc'
-    
-      
-    print(datetime.now())
-    average_end_members(covariates_grass_path, covariates_shrub_path, covariates_tree_path, RAP_in_dir, covariate_save_path, columns=['GPP', 'NPP', 'Ra', 'Rh', 'NEE'])
-    average_end_members(C_stocks_grass_path, C_stocks_shrub_path, C_stocks_tree_path, RAP_in_dir, C_stock_save_path)
-    print(datetime.now())
+def spatial_filter(netcdf_file, save_path, bucket, sd_threshold=10):
+
+  """
+  Spatial filtering for netCDF files with dimensions (time, y, x). Could also work on 2d data with
+  dimensions (y,x). Saves filtered dataset to Google Cloud.
+  
+  Args:
+  netcdf_file (str): path to netcdf
+  save_path (str): path to save result netcdf on google cloud bucket
+  bucket (google.cloud.storage.bucket): bucket to save output in
+  sd_threshold (numeric): None or subset of data variable names to process
+
+  Returns:
+     None
+  """
+  ds = rxr.open_rasterio(netcdf_file, masked=True)
+  
+  median = ds.median(dim=['x', 'y'])
+  std = ds.std(dim=['x', 'y'])
+  
+  ds = ds.where(abs(ds - median) < std * sd_threshold)
+  ds = ds.interpolate_na(dim=['y', 'x'], method="linear")
+  
+  utils.gs_write_blob(os.path.join(path_to_temp, 'temp_average_write.nc'), save_path, bucket)
+  utils.image_average_variables(ds, ['GPP', 'NEE', 'NPP', 'Ra', 'Rh'], time_index = 'time', plot_dir='/home/amullen/figs')
+  ds.to_zarr('gs://rangelands/Ranch_Runs/MCK/results/flux_hist_weighted_filtered.zarr')
+
+  
+#spatial_filter('gs://rangelands/Ranch_Runs/MCK/results/C_stock_hist_weighted.nc', 'Ranch_Runs/MCK/results/C_stock_hist_weighted_filtered.nc', bucket)
+spatial_filter('gs://rangelands/Ranch_Runs/MCK/results/flux_hist_weighted.nc', 'Ranch_Runs/MCK/results/flux_hist_weighted_filtered.nc', bucket)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
